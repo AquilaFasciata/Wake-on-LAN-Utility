018b885644c5480b4e305e1c33bef01fb68efa7c
Zach <zachisaunders12@gmail.com>
2020-03-10 12:11:36 -0400

    Basis

---
+++ src/appMain.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class appMain {
+    public setGui() {
+        Frame window = new Frame("Wake-on-LAN Utility");
+        window.setLocation(20, 20);
+
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
