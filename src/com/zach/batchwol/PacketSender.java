bdf47c38d3859a11117178d7d5be51ab5f6f60a4
Zach <zachisaunders12@gmail.com>
2020-03-31 15:24:48 -0400

    Finalized Magic Packet construction and sending

--- src/com/zach/batchwol/PacketSender.java
+++ src/com/zach/batchwol/PacketSender.java
@@ -1,9 +1,14 @@
 package com.zach.batchwol;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class PacketSender {
-    private final short PORT = 9;
+    // TODO Implement Broad Port Selection
+    // TODO Implement Port Selection for User
+    private static final int[] PORTS = {0, 7, 9};
+    private static final int DEFAULT_PORT = 9;
 
     public static void PacketSender(String macAddress) {
 
@@ -13,34 +18,40 @@ public class PacketSender {
         for (int i = 0; i < 6; i++)
             theMagic[i] = (byte) 0xff;
 
-        for (int i = 6; i < theMagic.length; i += macAddress.length()) {
+        for (int i = 6; i < theMagic.length; i += newMacs.length) {
             System.arraycopy(newMacs, 0, theMagic, i, newMacs.length);
         }
 
         try {
-            InetAddress broadcast = InetAddress.getByName("255.255.255.255");
+            InetAddress broadcast = InetAddress.getByName("192.168.1.255");
             System.out.println(broadcast.toString());
+            DatagramPacket packet = new DatagramPacket(theMagic, theMagic.length, broadcast, DEFAULT_PORT);
+            DatagramSocket socket = new DatagramSocket();
+            socket.send(packet);
+            socket.close();
         } catch (Exception e) {
             System.out.println(e);
         }
-
-        for (byte newByte: theMagic)
-            System.out.print(newByte);
     }
 
     private static byte[] MacToBytes(String address) {
         final char[] SEPERATORS = {':','-'};
         int lastIndex = 0;
-        String finalAddress = "";
+        byte[] result = new byte[6];
+        int j = 0;
         for (int i = 0; i < address.length(); i++) {
             if (address.charAt(i) == SEPERATORS[0] || address.charAt(i) == SEPERATORS[1]) {
-                finalAddress += address.substring(lastIndex, i);
+                final int parseInt = Integer.parseInt(address.substring(lastIndex, i), 16);
+                result[j] = (byte) parseInt;
                 lastIndex = i+1;
+                j++;
             }
         }
 
-
-        return finalAddress.getBytes();
+        System.out.println("Mac: ");
+        for (byte data: result)
+            System.out.print(data);
+        return result;
     }
 
     public static void main(String[] args) {
